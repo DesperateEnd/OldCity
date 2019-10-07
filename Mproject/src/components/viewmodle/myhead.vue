28a739263b1c55f47496b89553b22960c93b426a
Desperate1122 <desperate_jc@sina.com>
2019-10-07 09:35:14 +0800

    添加头部组件

---
+++ Mproject/src/components/viewmodle/myhead.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="allhead">
+      <span @click.stop="goback"><van-icon :name="left" /></span>
+      <span>{{text}}</span>
+      <span @click.stop="rightfun">{{righttype?'':right}}<van-icon :name="right" v-show="righttype"/></span>
+  </div>
+</template>
+
+<script>
+export default {
+name:'myhead',
+props:{
+    left:{
+        type:String,
+        default:''
+    },
+    text:{
+        type:String,
+        default:''
+    },
+    right:{
+        type:String,
+        default:''
+    },
+    righttype:{
+        type:Boolean,
+        default:true,
+    },
+    goback:{
+        type:Function,
+        default:this.goback
+    },
+    rightfun:{
+        type:Function,
+        default:()=>{},
+    }
+},
+data(){
+return{
+}
+},
+metheds:{
+    goback(){
+        this.$router.go(-1)
+    }
+},
+created(){
+},
+mounted(){
+}
+
+}
+
+</script>
+<style>
+</style>
