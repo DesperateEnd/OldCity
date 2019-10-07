5deb6bdb751db76de3f7bfac82d1c84e357667a1
Desperate1122 <desperate_jc@sina.com>
2019-10-07 09:34:52 +0800

    添加组件

---
+++ Mproject/src/components/Message.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="allbody massge">
+       <my-head  :text="'消息'" ></my-head>
+       <div class="allbox messagebg">
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'massge',
+    data(){
+        return{}
+    },
+    methods:{},
+    created(){},
+    mounted(){}
+}
+</script>
+
+<style>
+
+</style>
